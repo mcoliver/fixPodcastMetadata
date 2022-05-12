3b9b7c0c32a5c47271c664de310d3fb4d8cb65b1
Michael Oliver <mcoliver@gmail.com>
2022-05-11 20:09:10 -0700

    ready for public consumption

---
+++ README.md
@@ -0,0 +1,20 @@
+# Fix Podcasts Metadata
+I sync my Garmin Forerunner 645m with Apple Podcasts to have something to listen to when I run.  Unfortunately many of the podcast shows I listen to do not have the proper metadata embeded in the mp3s cached by Apple's Podcast app.  
+
+So on my watch I have a weird menu of nonsensical titles and episode names that make it impossible for me to locate the show I would like to listen to.  Using this script I can update the embeded metadata in the cached mp3s leveraging the data stored in Apple Podcasts sqlite database so on the next sync I get the proper experience on my watch.  
+
+I wish Garmin would integrate this into their Garmin Express product.  It has been a problem for years.  They have the logic to do it as the Garmin Express app clearly shows the metadata for the epsidoes in the GUI.  I have reached out to a few people over at garmin via linkedin but have not heard back.
+
+Original garmin reports [here](https://forums.garmin.com/sports-fitness/running-multisport/f/forerunner-645-645-m/208475/apple-podcasts-with-macos-10-15-catalina) and [here](https://forums.garmin.com/apps-software/mac-windows-software/f/garmin-express/281563/garmin-express-podcast-metadata-missing-causing-poor-user-experience).
+
+# Requirements
+* Python 3 ([https://www.python.org/downloads/](https://www.python.org/downloads/))
+* Mutagen `pip3 install mutagen`
+
+# Executing
+You can run it from the terminal before you run Garmin Express.  Or setup a cron job so that it runs in the background every so often (say once a day) to keep things up to date.
+
+```bash
+python3 -c "$(wget -O - https://github.com/mcoliver/fixPodcastsMetadata/fixPodcastMetadata.py)"
+
+```
---
+++ fixPodcastMetadata.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+#
+#  Add metadata from Apple Podcasts to cached mp3s
+#  so they sync to Garmin Watches with appropriate
+#  metadata
+#  ---------------
+#  Michael Oliver, 2022, MIT License
+#
+#  Standing on the shoulders of giants:
+#  Modified prior art and inspiration by Douglas Watson
+#  https://douglas-watson.github.io/post/2020-05_export_podcasts/
+#
+#  Intended for use as a cron job or to be run before Garmin Express
+#
+#  Queries the Apple Podcasts database for episodes that have been
+#  downloaded, then updates the metadata embeded in those files
+#  so that the mp3's have the correct metadata
+#
+#  https://mcoliver.com
+
+import os
+import urllib.parse
+import sqlite3
+
+SQL = """
+SELECT p.ZAUTHOR, p.ZTITLE, e.ZTITLE, e.ZASSETURL, e.ZPUBDATE
+from ZMTEPISODE e
+join ZMTPODCAST p
+    on e.ZPODCASTUUID = p.ZUUID
+where ZASSETURL NOTNULL;
+"""
+
+
+def check_imports():
+    ''' Prompts for password to install dependencies, if needed '''
+    try:
+        import mutagen
+    except ImportError:
+        os.system(
+            """osascript -e 'do shell script "/usr/bin/pip3 install mutagen" with administrator privileges'""")
+
+
+def get_downloaded_episodes(db_path):
+    '''Run SQL Query'''
+    return sqlite3.connect(db_path).execute(SQL).fetchall()
+
+
+def main(db_path):
+    '''Itterate through the database and re-encode the mp3s'''
+    for author, podcast, title, path, zpubdate \
+            in get_downloaded_episodes(db_path):
+
+        src_path = urllib.parse.unquote(path[len('file://'):])
+        print(f"Checking: {src_path}")
+        if os.path.exists(src_path):
+            try:
+                mp3 = MP3(src_path, ID3=EasyID3)
+                if mp3.tags is None:
+                    mp3.add_tags()
+                mp3.tags['artist'] = author
+                mp3.tags['album'] = podcast
+                mp3.tags['title'] = title
+                mp3.save()
+            except HeaderNotFoundError:
+                print(f"Corrupted file: {podcast} - {title}")
+                continue
+            except IsADirectoryError:
+                print(
+                    f"Failed to export {podcast} - {title}, media file is a movie")
+                continue
+            except FileNotFoundError:
+                print("File does not exist. skipping")
+                continue
+        else:
+            print (f"File does not Exist {src_path}")
+
+
+if __name__ == "__main__":
+    db_path = os.path.expanduser(
+        "~/Library/Group Containers/243LU875E5.groups.com.apple.podcasts/Documents/MTLibrary.sqlite")
+
+    check_imports()
+    from mutagen.mp3 import MP3, HeaderNotFoundError
+    from mutagen.easyid3 import EasyID3
+
+    main(db_path)
